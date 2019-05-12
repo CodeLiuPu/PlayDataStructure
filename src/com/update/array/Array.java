5a345c64d5638ef43180677913c3f55afeb005c9
liupu <575866447@qq.com>
2019-05-12 23:47:32 +0800

    Array add

--- src/com/update/array/Array.java
+++ src/com/update/array/Array.java
@@ -29,4 +29,30 @@ public class Array {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    public void add(int index, int e) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("add fail. Array is full.");
+        }
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add fail. Require index >= 0 & index <= size");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+
+        data[index] = e;
+        size++;
+
+    }
 }
