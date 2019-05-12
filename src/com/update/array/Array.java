776a35656a5f6a69311d638eaf46c7f2a8e84fcd
liupu <575866447@qq.com>
2019-05-12 21:27:10 +0800

    array

--- src/com/update/array/Array.java
+++ src/com/update/array/Array.java
@@ -6,4 +6,27 @@ package com.update.array;
  * desc : 自定义Array
  */
 public class Array {
+    private int[] data;
+    private int size;
+
+    public Array() {
+        this(10);
+    }
+
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
