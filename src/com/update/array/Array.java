7549e11e078f55accc261948fd3046f6649aa84d
liupu <575866447@qq.com>
2019-05-13 00:06:28 +0800

    Array toString

--- src/com/update/array/Array.java
+++ src/com/update/array/Array.java
@@ -55,4 +55,19 @@ public class Array {
         size++;
 
     }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array; size = %d , capacity = %d\n",size,data.length));
+        res.append("[");
+        for (int i=0;i<size;i++){
+            res.append(data[i]);
+            if (i!= size-1){
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
 }
