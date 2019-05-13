1fa3eefbf6ff2edc163dbd719521f7202b90a225
liupu <575866447@qq.com>
2019-05-13 17:52:21 +0800

    LinkedList add

--- src/com/update/list/LinkedList.java
+++ src/com/update/list/LinkedList.java
@@ -6,6 +6,51 @@ package com.update.list;
  * desc   :
  */
 public class LinkedList<E> {
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("add fail. legal index");
+        }
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
     private class Node {
         public E e;
         public Node next;
