66117a5561388400cd133593d68a2f6e5c0a24c2
liupu <575866447@qq.com>
2019-05-13 16:12:41 +0800

    ArrayQueue

---
+++ src/com/update/queue/ArrayQueue.java
@@ -0,0 +1,64 @@
+package com.update.queue;
+
+import com.update.array.Array;
+
+/**
+ * author : liupu
+ * date   : 2019/5/13
+ * desc   :
+ */
+public class ArrayQueue<E> implements Queue<E> {
+    private Array<E> data;
+
+    public ArrayQueue() {
+        data = new Array<>();
+    }
+
+    public ArrayQueue(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    @Override
+    public int getSize() {
+        return data.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int getCapacity() {
+        return data.getCapacity();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        data.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return data.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: ");
+        res.append("front [");
+        for (int i = 0; i < data.getSize(); i++) {
+            res.append(data.get(i));
+            if (i != data.getSize() - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+}
