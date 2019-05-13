d4194a9f0ef6946dac0aa3356740336dcfa53a97
liupu <575866447@qq.com>
2019-05-13 15:58:25 +0800

    Queue

---
+++ src/com/update/queue/Queue.java
@@ -0,0 +1,27 @@
+package com.update.queue;
+
+/**
+ * author : liupu
+ * date   : 2019/5/13
+ * desc   :
+ */
+public interface Queue<E> {
+    int getSize();
+
+    boolean isEmpty();
+
+    /**
+     * 入队
+     */
+    void enqueue(E e);
+
+    /**
+     * 出队
+     */
+    E dequeue();
+
+    /**
+     * 获取队首元素
+     */
+    E getFront();
+}
