dac6382158090175817ae340a4d72fb524c9864b
liupu <575866447@qq.com>
2019-05-17 16:11:08 +0800

    Binary-Search-Tree 删除 max min 节点

--- src/com/update/tree/BST.java
+++ src/com/update/tree/BST.java
@@ -191,7 +191,7 @@ public class BST<E extends Comparable<E>> {
     }
 
     /**
-     * 获取最小元素
+     * 获取最大元素
      */
     public E maximum() {
         if (size == 0) {
@@ -201,10 +201,59 @@ public class BST<E extends Comparable<E>> {
     }
 
     private E maximum(Node node) {
-       if (node.right == null){
-           return node.e;
-       }
+        if (node.right == null) {
+            return node.e;
+        }
         return maximum(node.right);
+    }
+
+    /**
+     * 从二分搜索树中删除最小值所在节点, 返回最小值
+     */
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    /**
+     * 删除掉以node为根的二分搜索树中的最小节点
+     * 返回删除节点后新的二分搜索树的根
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node right = node.right;
+            node.right = null;
+            size--;
+            return right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 从二分搜索树中删除最大值所在节点, 返回最大值
+     */
+    public E removeMax() {
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    /**
+     * 删除掉以node为根的二分搜索树中的最大节点
+     * 返回删除节点后新的二分搜索树的根
+     */
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
     }
 
     @Override
--- src/com/update/tree/Demo.java
+++ src/com/update/tree/Demo.java
@@ -7,11 +7,14 @@ package com.update.tree;
  */
 public class Demo {
     public static void main(String[] args) {
-        int[] arr = {5, 6, 2, 4, 8, 3, 1, 7, 9};
+//        int[] arr = {5, 6, 2, 4, 8, 3, 1, 7, 9};
+        int[] arr = {6, 3, 1, 2, 9};
         BST<Integer> bst = new BST<>();
         for (int i = 0; i < arr.length; i++) {
             bst.add(arr[i]);
         }
-        bst.preOrderNR();
+        System.out.println(bst);
+        bst.removeMin();
+        System.out.println(bst);
     }
 }
