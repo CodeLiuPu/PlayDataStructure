b335fdbf8b995a9140109eac2dbb9902916a1873
liupu <575866447@qq.com>
2019-05-22 13:08:13 +0800

    PriorityQueue

--- src/com/update/queue/PriorityQueue.java
+++ src/com/update/queue/PriorityQueue.java
@@ -2,6 +2,11 @@ package com.update.queue;
 
 import com.update.heap.MaxHeap;
 
+/**
+ * @author : liupu
+ * date   : 2019/5/21
+ * desc   : 优先队列 堆实现
+ */
 public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
 
     private MaxHeap<E> data;
