b51469cff4a4332d9fdd214ed0e6105eb0cc2864
liupu <575866447@qq.com>
2019-05-29 18:34:21 +0800

    AVLTree add

--- src/com/update/tree/AVLTree.java
+++ src/com/update/tree/AVLTree.java
@@ -33,13 +33,62 @@ public class AVLTree<K extends Comparable<K>, V> {
         return node.height;
     }
 
+    private int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.abs(getHeight(node.left) - getHeight(node.right));
+    }
+
     public void add(K key, V value) {
         root = add(root, key, value);
     }
 
     private Node add(Node node, K key, V value) {
+        if (null == node) {
+            size++;
+            return new Node(key, value);
+        }
 
-        return null;
+        int compareResuklt = key.compareTo(node.key);
+
+        if (compareResuklt == 0) {
+            node.value = value;
+        } else if (compareResuklt < 0) {
+            node.left = add(node.left, key, value);
+        } else {
+            node.right = add(node.right, key, value);
+        }
+        // 更新 height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor > 1) {
+            System.out.println("unbalance : " + balanceFactor);
+        }
+        return node;
+    }
+
+    /**
+     * 返回以node为根节点的二分搜索树中，key所在的节点
+     */
+    private Node getNode(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int compareResuklt = key.compareTo(node.key);
+        if (compareResuklt == 0) {
+            return node;
+        } else if (compareResuklt < 0) {
+            return getNode(node.left, key);
+        } else {
+            return getNode(node.right, key);
+        }
+    }
+
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
     }
 
     private class Node {
