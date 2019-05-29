8d3e54102c87941ab0027e673ccaa75a300d5459
liupu <575866447@qq.com>
2019-05-29 22:47:27 +0800

    AVLTree isBalanced

--- src/com/update/tree/AVLTree.java
+++ src/com/update/tree/AVLTree.java
@@ -24,7 +24,7 @@ public class AVLTree<K extends Comparable<K>, V> {
     }
 
     /**
-     * 是否为二分搜索树
+     * 判断该二叉树是否为二分搜索树
      */
     public boolean isBST() {
         // 中序遍历 判断是否key有序
@@ -46,6 +46,28 @@ public class AVLTree<K extends Comparable<K>, V> {
         inOrder(node.left, keys);
         keys.add(node.key);
         inOrder(node.right, keys);
+    }
+
+    /**
+     * 判断该二叉树是否为平衡二叉树
+     */
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    /**
+     * 判断该二叉树是否为平衡二叉树 递归算法
+     */
+    private boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
+        }
+
+        return isBalanced(node.left) && isBalanced(node.right);
     }
 
     public boolean isEmpty() {
