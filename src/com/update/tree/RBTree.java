d0e5dc5034af1f507aed3fb8fe2c78f1b14311cb
liupu <575866447@qq.com>
2019-06-02 23:58:09 +0800

    RBTree

--- src/com/update/tree/RBTree.java
+++ src/com/update/tree/RBTree.java
@@ -10,6 +10,39 @@ package com.update.tree;
  * 4. 如果一个节点是红色的,那么他的孩子节点都是黑色的
  * 5. 从任意一个节点到叶子节点,经过的黑色节点都是一样的
  */
-public class RBTree {
+public class RBTree<K extends Comparable<K>, V> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
 
+    private Node root;
+    private int size;
+
+    public RBTree() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left;
+        public Node right;
+        public boolean color;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            color = RED;
+        }
+    }
 }
